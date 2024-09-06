5f30576a44a823f3b7e77d8500e67c9ed86be4c0
Louise <louisegabrielvilar69@gmail.com>
2024-09-06 23:31:23 +0800

    Organize

--- CS32/LinkedList/18_problems/dynamic.c
+++ CS32/LinkedList/18_problems/dynamic.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 
 typedef struct node Node;
 struct node{
@@ -7,28 +8,39 @@ struct node{
     Node* next;
 };
 
-void freeLL(Node *head){ // {1,2,3}
-    Node *nextNode;
-    while (head != NULL){
-        nextNode = head->next;  // 2 3 
-        free(head); // 1
-        head = nextNode; // 2 3 
-    }
-}
+// void freeLL(Node *head){ // {1,2,3}
+//     Node *nextNode;
+//     while (head != NULL){
+//         nextNode = head->next;  // 2 3 
+//         free(head); // 1
+//         head = nextNode; // 2 3 
+//     }
+// }
 
 void displayLL(Node *head){
     while (head != NULL){
         printf(" %d-> ", head->val);
         head = head->next;
     }
+    printf("\n");
 }
+// int Pop(Node **head){
+//     if (*head == NULL){
+//         assert(0);
+//     }
+//     int popped;
+//     Node *current = *head;
+//     *head = current->next;
+//     popped = current->val;
+//     free(current);
+//     return popped;
+// }
 
-int main(){
+Node* Build(){
     int LL[] = {1,2,3,4,5};
     int len = sizeof(LL) / sizeof(LL[0]);
     Node *list = (Node*)malloc(sizeof(Node));
     Node *curr;
-    curr->next = NULL;
     list->next = NULL;
     for (int i = 0; i < len; i++){
         if (i==0){ // create head of list
@@ -39,13 +51,18 @@ int main(){
             // create every single node to link to list;
             Node *node = (Node*)malloc(sizeof(Node));
             node->val = LL[i];
+            node->next = NULL;
             curr->next = node; // link the node to the current head
             curr = node; // will become the new head
         }
     }
     curr->next = NULL; // curr will link to null; hence it will become the tail
 
+    return list;
+}
+
+int main(){
+    Node* list = Build();
     displayLL(list);
-    freeLL(list);
 
 }
--- helloworld.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    char a = 'a';  
-    int b = 1;
-    float c = 2.0f;
-    printf(" %c \n %d \n %f \n", a, b, c);
-    return 0;
-}
